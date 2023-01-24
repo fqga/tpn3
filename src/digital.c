8045503177466b9ed7af97edff6cede53eeb65b0
Facundo Q <facundoqga@gmail.com>
2023-01-24 00:23:39 -0300

    Inputs Ok

--- src/digital.c
+++ src/digital.c
@@ -59,6 +59,9 @@
     #define OUTPUT_INSTANCES    4
 #endif
 
+#ifndef INPUT_INSTANCES
+    #define INPUT_INSTANCES     6
+#endif 
 /* === Declaraciones de tipos de datos privados ============================ */
 struct digital_output_s 
 {
@@ -67,8 +70,18 @@ struct digital_output_s
     bool allocated;
 };
 
+struct digital_input_s
+{
+    uint8_t gpio;
+    uint8_t bit;
+    bool    inverted;
+    bool    last_state;
+    bool    allocated;
+};
+
 /* === Definiciones de variables privadas ================================== */
-static struct digital_output_s instance[OUTPUT_INSTANCES] = {0};
+static struct digital_output_s instance_ou[OUTPUT_INSTANCES] = {0};
+static struct digital_input_s  instance_in[INPUT_INSTANCES] = {0};
 
 /* === Definiciones de variables publicas ================================== */
 
@@ -82,14 +95,34 @@ digital_output_t DigitalOutputAllocate(void)
     digital_output_t output = NULL;
 
     for(int index = 0; index <  OUTPUT_INSTANCES; index++){
-        if (instance[index].allocated == false){
-            instance[index].allocated = true;
-            output = &instance[index];
+        if (instance_ou[index].allocated == false){
+            instance_ou[index].allocated = true;
+            output = &instance_ou[index];
             break;
         }
     }
 
     return output;
+}
+
+digital_input_t DigitalInputAllocate(void)
+{
+    digital_input_t input = NULL;
+
+
+    for (int index = 0; index < INPUT_INSTANCES; index++)
+    {
+
+        
+        if (instance_in[index].allocated == false)
+        {
+            instance_in[index].allocated = true;
+            input = &instance_in[index];
+            break;
+        }
+    }
+
+    return input;
 }
 
 /* === Definiciones de funciones publicas ================================== */
@@ -125,7 +158,49 @@ void DigitalOutputToggle(digital_output_t output)
 
 }
 
-/* === Ciere de documentacion ============================================== */
+digital_input_t DigitalInputCreate(uint8_t gpio, uint8_t bit, bool inverted)
+{
+    digital_input_t input = DigitalInputAllocate();
 
+    if (input)
+    {
+        input->gpio = gpio;
+        input->bit = bit;
+        input->inverted = inverted;
+        Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, input->gpio, input->bit, false);
+    }
+
+    return input;
+
+}
+
+bool DigitalInputGetState(digital_input_t input)
+{
+    return input->inverted ^ Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, input->gpio, input->bit);
+}
+
+bool DigitalInputHasChanged(digital_input_t input)
+{
+    bool state = DigitalInputGetState(input);
+    bool result = state != input->last_state;
+    input->last_state = state;
+    return result;
+}
+
+bool DigitalInputHasActivated(digital_input_t input)
+{
+    bool state = DigitalInputGetState(input);
+    bool result = state && !input->last_state;
+    input->last_state = state;
+    return result;
+}
+
+bool DigitalInputHasDeactivated(digital_input_t input)
+{
+    bool state = DigitalInputGetState(input);
+    bool result = !state && !input->last_state;
+    input->last_state = state;
+    return result;
+}
 /** @} Final de la definición del modulo para doxygen */
 
--- src/main.c
+++ src/main.c
@@ -123,7 +123,7 @@
 int main(void) {
 
     int divisor  = 0;
-    bool current_state, last_state = false;
+    //bool current_state, last_state = false;
 
     Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
     Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, false);
@@ -139,37 +139,44 @@ int main(void) {
 
     /******************/
     Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_1_GPIO, LED_1_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_1_GPIO, LED_1_BIT, true);
+    //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_1_GPIO, LED_1_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_1_GPIO, LED_1_BIT, true);
 
     Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, true);
+    //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, true);
 
     Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, true);
+    //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, true);
 
     /******************/
     Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT, false);
 
     Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_2_GPIO, TEC_2_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_2_GPIO, TEC_2_BIT, false);
 
     Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_3_GPIO, TEC_3_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_3_GPIO, TEC_3_BIT, false);
 
     Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT, false);
+    //Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT, false);
 
     digital_output_t led_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
     digital_output_t led_uno = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
     digital_output_t led_dos = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
     digital_output_t led_tres = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
 
+    digital_input_t  tecla_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, false);
+    digital_input_t  tecla_2 = DigitalInputCreate(TEC_2_GPIO,TEC_2_BIT, false);
+    digital_input_t  tecla_3 = DigitalInputCreate(TEC_3_GPIO,TEC_3_BIT, false);
+    digital_input_t  tecla_4 = DigitalInputCreate(TEC_4_GPIO,TEC_4_BIT, false);
+
+
+
     while (true) {
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT) == 0) {
+        if (DigitalInputGetState(tecla_1) == 0) {
             DigitalOutputActivate(led_azul);
             //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, true);
         } else {
@@ -177,17 +184,15 @@ int main(void) {
             //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, false);
         }
 
-        current_state = (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_2_GPIO, TEC_2_BIT) == 0);
-        if ((current_state) && (!last_state)) {
+        if (DigitalInputHasActivated(tecla_2)) {
             DigitalOutputToggle(led_uno);
         }
-        last_state = current_state;
 
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_3_GPIO, TEC_3_BIT) == 0) {
+        if (DigitalInputGetState(tecla_3) == 0) {
             DigitalOutputActivate(led_dos);
             //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, true);
         }
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT) == 0) {
+        if (DigitalInputGetState(tecla_4) == 0) {
             DigitalOutputDeactivate(led_dos);
             //Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, false);
         }
